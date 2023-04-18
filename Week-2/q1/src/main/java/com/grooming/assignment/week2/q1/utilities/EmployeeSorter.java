916e7ceacda87af7104d8866de9c855b79d2fe43
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-04-18 20:06:31 +0530

    Corrected logic to sort in descending order.

--- Week-2/q1/src/main/java/com/grooming/assignment/week2/q1/utilities/EmployeeSorter.java
+++ Week-2/q1/src/main/java/com/grooming/assignment/week2/q1/utilities/EmployeeSorter.java
@@ -11,9 +11,9 @@ public class EmployeeSorter implements Comparator<Employee> {
     @Override
     public int compare(Employee employee1, Employee employee2) {
          if(employee1.getJoiningDate().isBefore(employee2.getJoiningDate()))
-           return 1;
+           return -1;
          else if(employee1.getJoiningDate()==employee2.getJoiningDate())
           return 0;
-         return -1;
+         return 1;
     }
 }
